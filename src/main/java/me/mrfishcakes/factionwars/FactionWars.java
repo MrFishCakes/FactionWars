6016a7e6b2c7197d10134c25e4705d7090d46322
MrFishCakes <fishcake007@outlook.com>
2020-07-15 19:53:29 +0100

    Making use of Maven annotations by Lucko

--- src/main/java/me/mrfishcakes/factionwars/FactionWars.java
+++ src/main/java/me/mrfishcakes/factionwars/FactionWars.java
@@ -1,9 +1,20 @@
 package me.mrfishcakes.factionwars;
 
+import me.mrfishcakes.factionwars.maven.LibraryLoader;
+import me.mrfishcakes.factionwars.maven.MavenLibrary;
+import me.mrfishcakes.factionwars.maven.Repository;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@MavenLibrary(groupId = "com.github.JavaFactoryDev", artifactId = "LightningStorage", version = "3.1.5",
+        repo = @Repository(url = "https://jitpack.io"))
 public final class FactionWars extends JavaPlugin {
+
+    private static FactionWars instance;
+
+    public static FactionWars getInstance() {
+        return instance;
+    }
 
     @Override
     public void onEnable() {
@@ -12,10 +23,13 @@ public final class FactionWars extends JavaPlugin {
             getLogger().severe("installed on the server and is active!");
             return;
         }
+
+        instance = this;
+        LibraryLoader.loadAll(this);
     }
 
     @Override
     public void onDisable() {
-
+        instance = null;
     }
 }
